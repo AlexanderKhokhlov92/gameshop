d5e165f24bd117b170440f751aec376864c07460
Alexander Khokhlov <alh97495@gmail.com>
2024-09-24 12:56:00 +0700

    deploy commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Route, Routes, useNavigate } from "react-router-dom";
 import { useEffect } from "react";
 import Home from "./components/pages/home/Home";
 import GamePage from "./components/pages/game/GamePage";
@@ -7,15 +7,32 @@ import Cart from "./components/pages/cart/Cart";
 import SubscriptionPage from "./components/pages/subscriptionPage/SubscriptionPage";
 
 const App = () => {
+  const navigate = useNavigate();
+
   useEffect(() => {
     if (window.Telegram.WebApp) {
       window.Telegram.WebApp.ready(); // Уведомляем Telegram, что мини-приложение готово
+
+      // Показать кнопку "Назад" в интерфейсе Telegram Web App
+      window.Telegram.WebApp.BackButton.show();
+
+      // Обработчик нажатия кнопки "Назад"
+      window.Telegram.WebApp.BackButton.onClick(() => {
+        navigate(-1); // Возврат на предыдущую страницу
+      });
+
+      // Очистка при размонтировании
+      return () => {
+        window.Telegram.WebApp.BackButton.hide();
+        window.Telegram.WebApp.BackButton.offClick();
+      };
     }
-  }, []);
+  }, [navigate]);
 
   const closeApp = () => {
     window.Telegram.WebApp.close(); // Закрытие мини-приложения
   };
+
   return (
     <>
       <Routes>
